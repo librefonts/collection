6e71a5bc86ff644256578aab64f15118f8e378a2
Mikhail Kashkin <m@xen.ru>
2014-07-16 12:45:54 +0300

    apache fonts

--- process.sh
+++ process.sh
@@ -29,29 +29,30 @@ TTX="ttx"
 TEMPLATE="/Volumes/Fonts/template/"
 ALL="/Volumes/Fonts/all"
 startwd=$(pwd)
-echo "Delete files"
-find . -name "*.menu" -exec rm -rf {} \;
-find . -name "*.latin" -exec rm -rf {} \;
-find . -name "*.latin-ext" -exec rm -rf {} \;
-find . -name "*.greek" -exec rm -rf {} \;
-find . -name "*.greek-ext" -exec rm -rf {} \;
-find . -name "*.cyrillic" -exec rm -rf {} \;
-find . -name "*.cyrillic-ext" -exec rm -rf {} \;
-find . -name "*.vietnamese" -exec rm -rf {} \;
-find . -name "*.arabic" -exec rm -rf {} \;
-find . -name "*.ethiopic" -exec rm -rf {} \;
-find . -name "*.tamil" -exec rm -rf {} \;
-find . -name "*.thai" -exec rm -rf {} \;
-echo "Generate ttx files"
-find . -name "*.ttf" -exec ttx -s -o {}.ttx {} \;
-find . -name "*.otf" -exec ttx -s -o {}.ttx {} \;
-find . -name "*.ttf" -exec rm -rf {} \;
-find . -name "*.otf" -exec rm -rf {} \;
+# echo "Delete files"
+# find . -name "*.menu" -exec rm -rf {} \;
+# find . -name "*.latin" -exec rm -rf {} \;
+# find . -name "*.latin-ext" -exec rm -rf {} \;
+# find . -name "*.greek" -exec rm -rf {} \;
+# find . -name "*.greek-ext" -exec rm -rf {} \;
+# find . -name "*.cyrillic" -exec rm -rf {} \;
+# find . -name "*.cyrillic-ext" -exec rm -rf {} \;
+# find . -name "*.vietnamese" -exec rm -rf {} \;
+# find . -name "*.arabic" -exec rm -rf {} \;
+# find . -name "*.ethiopic" -exec rm -rf {} \;
+# find . -name "*.tamil" -exec rm -rf {} \;
+# find . -name "*.thai" -exec rm -rf {} \;
+# echo "Generate ttx files"
+# find . -name "*.ttf" -exec ttx -s -o {}.ttx {} \;
+# find . -name "*.otf" -exec ttx -s -o {}.ttx {} \;
+# find . -name "*.ttf" -exec rm -rf {} \;
+# find . -name "*.otf" -exec rm -rf {} \;
 
 for f in {apache,ofl,ufl}/*; do
     cd $startwd/$f
     # only name
     name=$(echo $f | sed 's/.*\///')
+    license=$(echo $f | sed 's/.\///' | sed 's/\/.*//')
     # echo "Process $name"
     # echo "curl -H \"Authorization: token $TOKEN\" -X DELETE https://api.github.com/repos/fontdirectory/$name" | bash
     echo "curl -H \"Authorization: token $TOKEN\" -d '{\"name\":\"$name\"}' -X POST https://api.github.com/orgs/fontdirectory/repos" | bash
@@ -61,7 +62,7 @@ for f in {apache,ofl,ufl}/*; do
     git remote add origin git@github.com:fontdirectory/$name.git
     git push -u origin master
     cd $ALL
-    git submodule add -f git://github.com/fontdirectory/$name.git $license/$name
+    git submodule add -f git://github.com/fontdirectory/$name.git $f
 done
 
 # hg revert --all
